da21ad8291f6f69f2bba5abfc7c19eeb9099e1fb
ninad-41 <76698136+ninad-41@users.noreply.github.com>
2021-06-05 18:03:00 +0530

    Update uploadingFiles.py

--- uploadingFiles.py
+++ uploadingFiles.py
@@ -1,33 +1,34 @@
+import os 
 import dropbox
-import os
+from dropbox.files import WriteMode
 
-
-class TransferData:
+class TransferData :
     def __init__(self, access_token):
-        self.aT = access_token
+        self.access_token = access_token
 
-    def upload_file(self, file_from):
-        db = dropbox.Dropbox(self.aT)
-        fileName = os.path.split(
-            file_from)[1]
+    def upload_file(self,dropbox_path,file_to, file_from, local_path,relative_path):
+        dbx = dropbox.Dropbox(self.access_token)
 
-        dropbox_file = '/CloudStorage/'+fileName
+        for root, dirs, files in os.walk(file_from):
+            relative_path = os.path.relpath(local_path,file_from)
+            dropbox_path = os.path.join(file_to,relative_path)
 
-        with open(file_from, "rb") as f:
-            db.files_upload(f.read(), dropbox_file,
-                            mode=dropbox.files.WriteMode.overwrite)
+        with open(local_path,'rb') as f:
+            dbx.files_upload(f.read(),dropbox_path,mode=WriteMode('overwrite'))
+
+def main():
+    access_token = 'sl.Ax-JPcE2Isl8bbmruOrJjU4YKOyBs5fmF3qmbtndGg2V4MqTfpraeZF5lbP2x8HIDHU61wqKQbJ28TeLe2P9nWfZkmcle_V9OhLtb1oJenxwRt-zDO1cPTSxhnKTwi0I5SjW9JVSNCG1'
+    transferData = TransferData(access_token)
+
+    file_from = input("Enter the file path to transfer : -")
+    file_to = input("enter the full path to upload to dropbox:- ")  
+    # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+    print("file has been moved !!!")
 
 
-AToken = "sl.ArS2rG_co_X3eSxdnce0i7XZyWbgtiuwjUcHIIV7uHSN2pDX0bjSJBzbScaSOaQkl3CW8yYfGYr2vMbY2xfzWqfbfU2IDXsevpYCDPVUEtjqT7_HR_PJpvtGbLEMnOgOEHqUNuY"
+main() 
 
-cloudStoring = TransferData(AToken)
-
-fileFrom = input("Please Give the File Path To Transfer ")
-
-while(os.path.isfile(fileFrom) == False):
-    print("Pls Give The Path Of A File")
-    fileFrom = input("Please Give the File Path To Transfer ")
-
-cloudStoring.upload_file(fileFrom)
-
-print("Files Have Been Stored On Dropbox ;)")
+              
